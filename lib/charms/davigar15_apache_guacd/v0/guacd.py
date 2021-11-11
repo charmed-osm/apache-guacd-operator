471727b2de0ddc66f1884a7fb7d2924ec353c2d1
Your Name <you@example.com>
2021-11-11 15:13:06 +0100

    Update patch of guacd charmlib

--- lib/charms/davigar15_apache_guacd/v0/guacd.py
+++ lib/charms/davigar15_apache_guacd/v0/guacd.py
@@ -56,7 +56,7 @@ LIBAPI = 0
 
 # Increment this PATCH version before using `charmcraft publish-lib` or reset
 # to 0 if you are raising the major API version
-LIBPATCH = 1
+LIBPATCH = 2
 
 
 def pod_ip() -> Optional[IPv4Address]:
